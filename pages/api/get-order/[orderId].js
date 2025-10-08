d5d64ed8867b52d3510a4dfa6e4bf522634ec9ac
sareaydin361-eng <sareaydin361@gmail.com>
2025-10-08 14:09:48 +0530

    Update [orderId].js

--- pages/api/get-order/[orderId].js
+++ pages/api/get-order/[orderId].js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
       return res.status(200).end(); // Preflight OK
     }
   const { orderId } = req.query;
+  console.log("Fetching order:", orderId);
 
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
@@ -21,6 +22,8 @@ export default async function handler(req, res) {
     await connectDB(); // ensure DB connection
 
     const order = await Order.findOne({ orderId });
+
+    console.log("Fetched order from DB:", order);
 
     if (!order) {
       return res.status(404).json({ error: "Order not found" });
